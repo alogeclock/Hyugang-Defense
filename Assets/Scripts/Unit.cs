10944843daf98ec2c3806bba7cfb8921a5a6d51a
alogeclock <alogeclock@gmail.com>
2024-11-26 03:09:19 +0900

    *

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -86,6 +86,7 @@ public class Unit : MonoBehaviour
         // 레벨 증가
         // 체력 증가
         // 공격 속도 증가
+        // price 값에 따라 업그레이드 가격 증가
         // 업그레이드 가격에 따라 price 값 증가 (판매 시 주는 돈 증가)
         // if (isFarm) earn 값 증가
     }
