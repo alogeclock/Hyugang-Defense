f02a9b49a410b19f6d17e5f54f3e7d2ca4549518
alogeclock <alogeclock@gmail.com>
2024-12-02 16:22:16 +0900

    *

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using JetBrains.Annotations;
 using TMPro;
 using Unity.VisualScripting;
-using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
